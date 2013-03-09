be97310f02402a87fe87fed23d9680bf767cd086
Kris <andagi@gmail.com>
2013-03-09 08:53:21 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mwit::Application.config.secret_token = 'f3f45851dccc45505874e358ce8e653c2a5f634fbca3a5fc1ab8179efd32137d9c0c86af93401dbb50d627282aaf88ba0dd166622b4df36b0d1e35b902cf55aa'
