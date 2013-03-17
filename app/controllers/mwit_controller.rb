0fc55e44b1d8299d9c13507cdcf12ce3e1f1c441
Kris <andagi@gmail.com>
2013-03-17 16:57:11 -0700

    Initial Front Page

---
+++ app/controllers/mwit_controller.rb
@@ -0,0 +1,2 @@
+class MwitController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Mwit::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :mwit
+  root :to => redirect('/mwit')
 
   # Sample resource route with options:
   #   resources :products do
